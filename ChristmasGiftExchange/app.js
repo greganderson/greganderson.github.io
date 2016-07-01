7bb7d5feda3934e55d739ce6ec8e62c4f0d6096a
Greg Anderson <greg@infobytes.com>
2016-07-01 16:01:15 -0600

    Added some event test code

--- ChristmasGiftExchange/app.js
+++ ChristmasGiftExchange/app.js
@@ -9,17 +9,24 @@ function christmasController() {
     var ctrl = this;
 
     ctrl.familyList = [];
-
-    ctrl.myfunc = function() {
-        console.log('got called');
-        console.log(ctrl.numFamilies)
-    };
+    ctrl.numFamilies = 10;
 
     ctrl.range = function(max) {
+        ctrl.familyList = [];
         var input = [];
         for (var i = 1; i <= max; i++) {
             input.push(i);
+            ctrl.familyList.push(i);
         }
         return input;
+    }
+
+    ctrl.blamo = function() {
+        console.log('yay');
+    }
+
+    ctrl.wham = function() {
+        console.log(ctrl.familyList);
+        console.log(ctrl.stuff);
     }
 }
