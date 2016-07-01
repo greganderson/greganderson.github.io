11dbc7a195a1210580c0796437b9d309b4de4c03
Greg Anderson <greg@infobytes.com>
2016-07-01 15:03:20 -0600

    Dynamically add/remove rows

--- ChristmasGiftExchange/app.js
+++ ChristmasGiftExchange/app.js
@@ -8,7 +8,18 @@ christmasApp.component('christmasApp', {
 function christmasController() {
     var ctrl = this;
 
+    ctrl.familyList = [];
+
     ctrl.myfunc = function() {
         console.log('got called');
+        console.log(ctrl.numFamilies)
     };
+
+    ctrl.range = function(max) {
+        var input = [];
+        for (var i = 1; i <= max; i++) {
+            input.push(i);
+        }
+        return input;
+    }
 }
